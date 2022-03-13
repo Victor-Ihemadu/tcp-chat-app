a91e83eb1c89831205be7db8c8c0b94dbeb954b1
Victor-Ihemadu <victorihemadu@gmail.com>
2022-03-13 16:06:52 +0100

    made changes

--- tcp-chat/client/main.go
+++ tcp-chat/client/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+)
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+var (
+	openConnections = make(map[net.Conn]bool)
+	newConnection   = make(chan net.Conn)
+	deadConnection  = make(chan net.Conn)
+)
 
 func main() {
-	fmt.Println("test client")
+	ln, err := net.Listen("tcp", ":8080")
+	logFatal(err)
+
+	defer ln.Close()
+
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			logFatal(err)
+
+			openConnections[conn] = true
+			newConnection <- conn
+		}
+	}()
+
 }
--- tcp-chat/server/main.go
+++ tcp-chat/server/main.go
@@ -11,6 +11,26 @@ func logFatal(err error) {
 	}
 }
 
+var (
+	openConnections = make(map[net.Conn]bool)
+	newConnection   = make(chan net.Conn)
+	deadConnection  = make(chan net.Conn)
+)
+
 func main() {
 	ln, err := net.Listen("tcp", ":8080")
+	logFatal(err)
+
+	defer ln.Close()
+
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			logFatal(err)
+
+			openConnections[conn] = true
+			newConnection <- conn
+		}
+	}()
+
 }
